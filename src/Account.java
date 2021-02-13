72af812e7550ab4cde7f5541599c4108ff4bcbc9
adarSonmez <adarsonmez@outlook.com>
2021-02-13 18:21:19 +0300

    updated

--- src/Account.java
+++ src/Account.java
@@ -1,59 +1,59 @@
 import java.util.Date;
 
 public class Account {
-	// Data fields
-	private static final double MONTHLY_INTEREST_RATE = 8.24;
-	private static int numberOfAccounts = 0;
-	private final int id;
-	private double balance;
-	private long password;
-	private final long dateCreated;
+    // Data fields
+    private static final double MONTHLY_INTEREST_RATE = 8.24;
+    private static int numberOfAccounts = 0;
+    private final int id;
+    private final long dateCreated;
+    private double balance;
+    private long password;
 
-	// Constructor
-	Account(int id, double newBalance) {
-		this.id = id;
-		balance = newBalance;
-		password = 0; // default password
-		dateCreated = new Date().getTime();
-		numberOfAccounts++;
-	}
+    // Constructor
+    Account(int id, double newBalance) {
+        this.id = id;
+        balance = newBalance;
+        password = 0; // default password
+        dateCreated = new Date().getTime();
+        numberOfAccounts++;
+    }
 
-	// Mutator methods
-	public void setPassword(long newPassword) {
-		password = newPassword;
-	}
+    public static int getNumberOfAccounts() {
+        return numberOfAccounts;
+    }
 
-	public double setBalance(double balance) {
-		long currentTime = new Date().getTime();
-		double interestedAmount = (int)(currentTime - dateCreated) / 1000.0 * (MONTHLY_INTEREST_RATE / 2_629_743);
-		return balance + interestedAmount;
-	}
+    public double setBalance(double balance) {
+        long currentTime = new Date().getTime();
+        double interestedAmount = (int) (currentTime - dateCreated) / 1000.0 * (MONTHLY_INTEREST_RATE / 2_629_743);
+        return balance + interestedAmount;
+    }
 
-	// Accessor methods
-	public int getId() {
-		return id;
-	}
+    // Accessor methods
+    public int getId() {
+        return id;
+    }
 
-	public double getBalance() {
-		balance = setBalance(balance);
-		return balance;
-	}
+    public double getBalance() {
+        balance = setBalance(balance);
+        return balance;
+    }
 
-	public long getPassword() {
-		return password;
-	}
+    public long getPassword() {
+        return password;
+    }
 
-	public static int getNumberOfAccounts() {
-		return numberOfAccounts;
-	}
+    // Mutator methods
+    public void setPassword(long newPassword) {
+        password = newPassword;
+    }
 
-	// Decrease the balance by the given amount
-	public void withdraw(double amount) {
-		balance -= amount;
-	}
+    // Decrease the balance by the given amount
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
 
-	// Increase the balance by the given amount
-	public void deposit(double amount) {
-		balance += amount;
-	}
+    // Increase the balance by the given amount
+    public void deposit(double amount) {
+        balance += amount;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         GregorianCalendar calendar = new GregorianCalendar();
         int hourOfDay = calendar.get(GregorianCalendar.HOUR_OF_DAY);
@@ -13,26 +16,30 @@ public class Main {
         // Create 100 accounts in an array
         int id = 0;
         Account[] accounts = new Account[1000];
+        File file = new File("accounts.txt");
+        file.delete();
+        file.createNewFile();
 
         // Determine the working hours of the system
-        while (hourOfDay >= 9 && hourOfDay < 17) {
+        while (hourOfDay >= 9 && hourOfDay < 22
+        ) {
             System.out.print("Enter 1 to create a new account, or enter 2 to trade your current account: ");
             int userStatus = input.nextInt();
-            while (userStatus != 1 && userStatus != 2) {
+            while (userStatus != 1 && userStatus != 2 && userStatus != 3) {
                 System.out.print("Invalid input, enter again: ");
                 userStatus = input.nextInt();
             }
 
             if (userStatus == 1) {
                 // Determine user limit
-                if (id >= 999){
+                if (id >= 999) {
                     System.out.println("Sorry, we cannot accept a new user.");
                 } else {
-                    newAccount(accounts, id, input);
+                    newAccount(accounts, id, input, file);
                     id++;
                 }
             } else {
-                currentAccount(accounts, input);
+                currentAccount(accounts, input, file);
             }
             // Once you exit, the system will prompt for an id again
             hourOfDay = calendar.get(GregorianCalendar.HOUR_OF_DAY);
@@ -42,7 +49,7 @@ public class Main {
         System.exit(0);
     }
 
-    public static void newAccount(Account[] a, int id, Scanner input) {
+    public static void newAccount(Account[] a, int id, Scanner input, File file) throws IOException {
         a[id] = new Account(id, 0);
         System.out.print("Set your password (must be a 4-digit number): ");
         long password = input.nextInt();
@@ -51,9 +58,11 @@ public class Main {
         System.out.println("You have successfully registered in our system.");
         System.out.println("Your id is " + a[id].getId());
         System.out.println("Your password is " + password);
+
+        write(id, 0, file);
     }
 
-    public static void currentAccount(Account[] accounts, Scanner input) {
+    public static void currentAccount(Account[] accounts, Scanner input, File file) throws IOException {
         // Prompt user to enter an id
         System.out.print("Enter your id: ");
         int id = input.nextInt();
@@ -66,7 +75,7 @@ public class Main {
                 // Get user choice
                 choice = displayMainMenu(input);
                 if (isTransaction(choice)) {
-                    executeTransaction(choice, accounts, id, input);
+                    executeTransaction(choice, accounts, id, input, file);
                 }
             } while (choice != 4); // If 4 exit main menu
         }
@@ -97,7 +106,7 @@ public class Main {
         return input.nextInt();
     }
 
-    public static void executeTransaction(int c, Account[] a, int id, Scanner input) {
+    public static void executeTransaction(int c, Account[] a, int id, Scanner input, File file) throws IOException {
         switch (c) {
             // Viewing the current balance
             case 1 -> System.out.println("The balance is " + a[id].getBalance());
@@ -116,5 +125,12 @@ public class Main {
                 a[id].deposit(input.nextDouble());
             }
         }
+        write(id, a[id].getBalance(), file);
+    }
+
+    public static void write(int id, double balance, File file) throws IOException {
+        FileWriter printer = new FileWriter(file, true);
+        printer.write("ID: " + id + ", Balance: " + balance + "\n");
+        printer.close();
     }
 }
